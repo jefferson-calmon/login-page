f5f55826ce1f40e96d2ec973d0780f1bfbe3d321
jefferson-calmon <62778628+jefferson-calmon@users.noreply.github.com>
2020-06-16 17:45:16 -0300

    Estilizando alguns elementos com CSS

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -5,7 +5,32 @@ import './style.css';
 const Login = () => {
     return(
         <div id="container">
-            <h1>login page</h1>
+            
+            <div id="box">
+                <form>
+                    <h2>Login</h2>
+                    <div className="row">
+                        <input type="text" name="" required/>
+                        <label className="text">
+                            Usuário
+                        </label>
+                        <span className="line"></span>
+                        <span className="line2"></span>
+                    </div>
+
+                    <div className="row">
+                        <input type="password" required/>
+                        <label className="text">
+                            Senha
+                        </label>
+                        <span className="line"></span>
+                        <span className="line2"></span>
+                    </div >
+
+                    <input type="submit"/>
+                </form>
+            </div>
+
         </div>
     )
 }
